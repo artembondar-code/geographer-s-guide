1294565f14f4134b18aded3531198f2a7f093f54
artembondar-code <artem.bondar@nure.ua>
2026-05-11 15:17:35 +0300

    Delete Model.cs

--- Model.cs
+++
@@ -1,9 +0,0 @@
-namespace GeographerGuide
-{
-    public class City
-    {
-        public string Name { get; set; } = string.Empty;
-        public string Population { get; set; } = string.Empty;
-        public string Coordinates { get; set; } = string.Empty;
-    }
-}
